2b5a13beec0b498a878912986704306ad5825e56
DeniseBergmann <denichabepa@gmail.com>
2024-03-14 20:28:07 -0500

    Cambios

---
+++ src/app/autos/lista_autos/lista_autos.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { Lista_autosComponent } from './lista_autos.component';
+
+describe('Lista_autosComponent', () => {
+  let component: Lista_autosComponent;
+  let fixture: ComponentFixture<Lista_autosComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ Lista_autosComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Lista_autosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/autos/lista_autos/lista_autos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista_autos',
+  templateUrl: './lista_autos.component.html',
+  styleUrls: ['./lista_autos.component.css']
+})
+export class Lista_autosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
