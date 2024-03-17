cf32cdfea15d363c8e91ad0e7fc849627553004b
DeniseBergmann <denichabepa@gmail.com>
2024-03-17 16:38:09 -0500

    CAMBIOS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Lista_autosComponent } from './autos/lista_autos/lista_autos.component';
 import { FormsModule } from '@angular/forms';
-import { TraducePipe } from './traduce.pipe';
+import { TraducePipe } from './shared/traduce.pipe';
+import { AEspacioPipe } from './shared/a-espacio.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     Lista_autosComponent,
-    TraducePipe
+    TraducePipe,
+    AEspacioPipe
 
   ],
   imports: [
--- src/app/traduce.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TraducePipe } from './traduce.pipe';
-
-describe('TraducePipe', () => {
-  it('create an instance', () => {
-    const pipe = new TraducePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
